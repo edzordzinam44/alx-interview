56c29d54bb8698dfaedf8eedbb505e1461068810
edzordzinam44 <edzordzinam.alipui@gmail>
2024-06-10 18:09:01 +0000

    PAscal triangle iteration

---
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+"""
+0-Pascal's Triangle
+"""
+
+def pascal_triangle(n):
+    """Returns a list of lists of integers representing the Pascal’s triangle of n"""
+    if n <= 0:
+        return []
+    
+    triangle = []
+    
+    for i in range(n):
+        if i == 0:
+            # First row is always [1]
+            row = [1]
+        else:
+            # Generate current row based on the previous row
+            row = [1]
+            for j in range(1, i):
+                row.append(triangle[i-1][j-1] + triangle[i-1][j])
+            row.append(1)
+        
+        triangle.append(row)
+    
+    return triangle
